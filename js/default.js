3922ce778ef416a55da742174ea2f346d2497568
紅月九音 <teiruzuof.15@gmail.com>
2017-07-03 18:45:58 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -17,6 +17,7 @@ document.getElementById('countdown').textContent =
   counter[2] + '時' +
   counter[1] + '分' +
   counter[0] + '秒' ;
+}
 
 document.getElemenById('form').select.onchange = function(){
    location.href = document.getElementById('form').select.value;
