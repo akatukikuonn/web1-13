a75c8069ae356d3e00ed7029315f2ee7d5e2239e
紅月九音 <teiruzuof.15@gmail.com>
2017-07-03 17:37:06 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -56,5 +56,5 @@ else{document.getElementById('cookie').textContent ='はじめまして！';
     }
 
 var current_time = new Date();
-setCookie(lastDate', current_time.toString(17:36), 7);
+setCookie(lastDate', current_time.toString(), 7);
           
