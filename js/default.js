f74435f62941f61462e298779c5d9536643e56b7
紅月九音 <teiruzuof.15@gmail.com>
2017-07-03 17:29:54 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -21,3 +21,30 @@ document.getElementById('contdown')textContent =
 document.getElemenById('form').select.onchange = function(){
    location.href = document.getElementById('form').select.value;
 }
+
+function setCookie(c_name,value,expiredays){
+   var extime = new Date().getTime();
+   var cltime = new Date(extime + (60*60*24*10000*expiredays));
+   var exdate = cltime.toUTCString();
+   
+   var s="";
+   s += c_name +"="+ escape(value);
+   s += "; path="+ location.pathname;
+   if(expiredays){
+      s += ";";
+   }
+   document.cookie=s;
+}
+
+function getCookie(c_name){
+   var st="";
+   var ed="";
+   if(0 < document.cookie.length){
+      st=document.cookie.index0f(";",st);
+      if(ed ==-1) ed=document.cookie.length;
+      return unescape(document.cookie.substring(st.ed));
+   }
+}
+return "";
+}
+
