00cb5f011f35c53d1dd88dd00cc0149615579006
紅月九音 <teiruzuof.15@gmail.com>
2017-07-03 17:12:30 +0900

    Update default.js

--- js/default.js
+++ js/default.js
@@ -1,2 +1,22 @@
+var separate_time = function(time){
+   var sec  = time.getSeconds();
+   var min  = time.getMinutes();
+   var hours = time.getHours();
+   var days = time.getDate();
+   var month = time.getMonth();
+   var year  = time.getFullYear();
+  return [sec,min,hours,days,month,year];
+}
+
+var now = new Date();
+var counter = separate_time(now);
+document.getElementById('contdown')textContent = 
+  counter[5] + '年' +
+  conuter[4] + '月' +
+  conuter[3] + '日' +
+  conuter[2] + '時' +
+  conuter[1] + '分' +
+  conuter[0] + '秒' ;
+
 function getFileName(){
   return window.location.href
